e922f311e1a9de7b0dc199408c7b8c76f2b307f8
JavierGarciaUCLM <JavierGarciaUCLM@users.noreply.github.com>
2025-01-30 19:54:58 +0000

    deploy: 69a5bec86ac63d9a1c0377a8eaed8cc0ade88297

--- src/config.ts
+++
@@ -1,7 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Javier Personal Portfolio Website';
-export const SITE_DESCRIPTION = 'Welcome to my personal portfolio, place to find all my projects and personal information!';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
