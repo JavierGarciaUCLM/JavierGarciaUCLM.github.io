3d73dbd46e896e2a8470132bf85d13bfa87dc0d8
JavierGarciaUCLM <JavierGarciaUCLM@users.noreply.github.com>
2025-01-31 15:56:55 +0000

    deploy: 5a636e38c3b7eaca23a80ad08b27f41f1e84d3d0

---
+++ _astro/hoisted.CucfbCEe.js
@@ -0,0 +1,7 @@
+import"./hoisted.rasoniT7.js";async function e(){const t=await(await fetch("https://api.github.com/repos/JavierGarciaUCLM/Jobder")).json();document.getElementById("github-repo").innerHTML=`
+        <h3 class="text-xl font-semibold">${t.full_name}</h3>
+        <p>${t.description||"No description available."}</p>
+        <p><strong>Last updated:</strong> ${new Date(t.updated_at).toLocaleDateString()}</p>
+        <p><strong>Stars:</strong> ${t.stargazers_count} ⭐</p>
+        <a href="${t.html_url}" target="_blank" class="text-blue-600 underline hover:text-blue-800 dark:text-blue-400">View on GitHub</a>
+      `}e();
